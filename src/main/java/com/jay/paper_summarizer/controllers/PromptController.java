3ebd51a5a06e81dc84bf4df4f20c7aeacb03596d
Jay <jayandratodawat@gmail.com>
2024-11-02 13:38:52 +0530

    Feat : Implemented SSE for /prompt
    Implemented SSE for creating stream for chatmodel response and sent to client as response is generated.

--- src/main/java/com/jay/paper_summarizer/controllers/PromptController.java
+++ src/main/java/com/jay/paper_summarizer/controllers/PromptController.java
@@ -2,21 +2,26 @@ package com.jay.paper_summarizer.controllers;
 
 import com.jay.paper_summarizer.dto.PromptDTO;
 import com.jay.paper_summarizer.services.PromptService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/paper_summarizer")
 public class PromptController {
 
-    @Autowired
-    private PromptService promptService;
+    final private PromptService promptService;
 
-    @PostMapping("/prompt")
-    public String getPrompt(@RequestBody PromptDTO promptDTO) {
-        return promptService.callPromptService(promptDTO.prompt());
+    @PostMapping(value = "/prompt", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> getPrompt(@RequestBody PromptDTO promptDTO) {
+        Flux<String> response = promptService.callPromptService(promptDTO.prompt());
+        return response.map(message -> ServerSentEvent.builder(message).build());
     }
 }
--- src/main/java/com/jay/paper_summarizer/services/PromptService.java
+++ src/main/java/com/jay/paper_summarizer/services/PromptService.java
@@ -1,23 +1,17 @@
 package com.jay.paper_summarizer.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import reactor.core.publisher.Flux;
 
 
 @Service
 @AllArgsConstructor
 public class PromptService {
     private final ChatModel chatModel;
-    public String callPromptService(String prompt) {
-        return chatModel.call(prompt);
+    public Flux<String> callPromptService(String prompt) {
+        return chatModel.stream(prompt);
     }
 
 }
